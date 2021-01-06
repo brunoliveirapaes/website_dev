1814e0301a9d78b1c7391450f609a65994603482
brunoliveirapaes <bruno.paes@buzzbeat.pt>
2021-01-06 11:47:41 +0000

    3rd submit

--- js/main.js
+++ js/main.js
@@ -60,4 +60,28 @@ var slider = {
   slider.init();
 
 
+  /* Owl Carousel.js */
+
+  $('.owl-carousel').owlCarousel({
+    items:3,
+    loop:true,
+    margin:70,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+
+
 
